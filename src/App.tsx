aaca53faa92b6b8e865967105d59250e359abb81
Benkabo <kaboyokabenedict@gmail.com>
2024-06-21 12:26:30 +0300

    New changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,32 @@ import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <div></div>
-    </>
+    <div>
+      <div className=" h-screen flex items-center justify-center">
+        <div className="">
+          <div className="text-center mb-5">Hello &#x1F44B; I'm</div>
+          <div className="text-center text-4xl font-bold mb-5">
+            Benedict Kaboyoka
+          </div>
+          <div>
+            <div className="underline">Skills</div>
+            <p className="mb-3">
+              Web Development,Mobile Development,Debugging, Problem Solving
+            </p>
+            <div className="underline">Active Toolset</div>{" "}
+            <p className="mb-3">
+              JavaScript, Go ( Learning ), Typescript, React, React Native
+            </p>
+            <div className="underline">Other Toolset (Not actively using)</div>{" "}
+            <p className="mb-3">Vue, Python</p>
+            <div>
+              <span className="underline mr-2">Get In Touch</span>👇
+            </div>
+          </div>
+          <Navbar />
+        </div>
+      </div>
+    </div>
   );
 }
 
---
+++ src/components/Switch.tsx
@@ -0,0 +1,7 @@
+export default function Switch() {
+  return (
+    <div className="absolute top-5 right-5">
+      <button className="bg-blue-700 p-5 rounded-full"></button>
+    </div>
+  );
+}
